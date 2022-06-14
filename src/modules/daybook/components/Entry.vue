d3cef4a8788b9173ed7fa37c468b2411e34e0b89
Jose Burgon <burgonjose@gmail.com>
2022-06-14 16:30:22 -0400

    Avanzando hasta la clase #163

---
+++ src/modules/daybook/components/Entry.vue
@@ -0,0 +1,38 @@
+<template>
+  <div @click="$router.push({ name: 'entry', params: { id: 10 } })" class="entry-container mb-3 pointer p-2">
+    <div class="entry-title d-flex">
+      <span class="text-success fs-5 fw-bold">15</span>
+      <span class="mx-1 fs-5">Julio</span>
+      <span class="mx-2 fw-light">2021, jueves</span>
+    </div>
+
+    <div class="entry-description">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+      molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+      numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+      optio, eaque rerum! Provident similique accusantium nemo autem.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DaybookEntry'
+}
+</script>
+
+<style lang="scss" scoped>
+.entry-container {
+  border-bottom: 1px solid #2c3e50;
+  transition: 0.2s all ease-in;
+
+  &:hover {
+    background-color: lighten($color: grey, $amount: 45);
+    transition: 0.2s all ease-in;
+  }
+
+  .entry-description {
+    font-size: 12px;
+  }
+}
+</style>
---
+++ src/modules/daybook/components/EntryList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="entry-list-container">
+    <div class="p-2">
+      <input type="text" name="" id="" class="form-control" placeholder="Buscar entrada">
+    </div>
+
+    <div class="entry-scroll-area">
+      <Entry v-for="item in 100" :key="item" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+
+export default {
+  name: 'EntryList',
+  components: {
+    Entry: defineAsyncComponent(() => import('@/modules/daybook/components/Entry.vue'))
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.entry-list-container {
+  border-right: 1px solid #2c3e50;
+  height: calc(100vh - 56px);
+}
+
+.entry-scroll-area {
+  height: calc(100vh - 110px);
+  overflow: scroll;
+}
+</style>
